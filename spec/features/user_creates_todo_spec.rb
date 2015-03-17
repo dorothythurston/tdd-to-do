3880cf3dec2f70b72b64362a37ecc694bf0a3978
Dorothy Thurston <dorothy.thur@gmail.com>
2015-03-17 16:31:29 -0400

    User can sign in

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
+require "support/features/sign_in"
 
 feature "User creates todo" do
   scenario "successfully" do
-    visit root_path
+    sign_in
 
     click_on "Add a new todo"
     fill_in "Title", with:"Buy Milk"
