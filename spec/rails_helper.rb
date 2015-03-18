e8e1fdc990a935f64927e925e0eef18189efac8e
Dorothy Thurston <dorothy.thur@gmail.com>
2015-03-17 21:11:32 -0400

    User only sees user's todos

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,6 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'support/features/sign_in'
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
