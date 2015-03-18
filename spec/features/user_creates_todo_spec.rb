b6d41b8cee037806ab2742387c85b53aabd96da8
Dorothy Thurston <dorothy.thur@gmail.com>
2015-03-18 14:13:30 -0400

    Refactor tests to use helper methods

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -5,10 +5,8 @@ feature "User creates todo" do
   scenario "successfully" do
     sign_in
 
-    click_on "Add a new todo"
-    fill_in "Title", with:"Buy Milk"
-    click_on "Submit"
+    create_todo "Buy Milk"
 
-    expect(page).to have_css ".todos li", text: "Buy Milk"
+    expect(page).to display_todo "Buy Milk"
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'support/features/sign_in'
+require 'support/features/todo_helper'
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
